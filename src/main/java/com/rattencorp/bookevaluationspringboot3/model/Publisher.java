e8a9b47030cf562c7cd97cb31376ec5763b2154b
leuchtstoffratte <kitersquirel@web.de>
2025-02-09 01:20:35 +0100

    simplified publisher and removed JPA-style many-to-one-fields

--- src/main/java/com/rattencorp/bookevaluationspringboot3/model/Publisher.java
+++ src/main/java/com/rattencorp/bookevaluationspringboot3/model/Publisher.java
@@ -2,8 +2,7 @@ package com.rattencorp.bookevaluationspringboot3.model;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
+
 
 public class Publisher implements Serializable {
 
@@ -11,33 +10,15 @@ public class Publisher implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private final String publisherName;
-    private final Set<BookEdition> books;
-    private final Set<Author> authors;
 
 
     public Publisher(String name) {
         this.publisherName = name;
-        this.books = new HashSet<>();
-        this.authors = new HashSet<>();
-    }
-
-    public Set<BookEdition> getBooks() {
-        return books;
-    }
-
-    public Set<Author> getAuthors() {
-        return authors;
-    }
-
-    public void addBook(BookEdition book) {
-        books.add(book);
-    }
-
-    public void addAuthor(Author author) {
-        authors.add(author);
     }
 
     public String getPublisherName() {
         return publisherName;
     }
+
+
 }
