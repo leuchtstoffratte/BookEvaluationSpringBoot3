46cf58ce3fa51cd5febe01ae38a6029057855eaa
leuchtstoffratte <kitersquirel@web.de>
2025-02-09 01:24:01 +0100

    shell script with curl-commands to test rest-controller

---
+++ shellScripts/insertStandardBook.sh
@@ -0,0 +1,27 @@
+#BookEdition:
+#{
+#	"book":{
+#			"title":"effective java",
+#			"author":{
+#				"name":"martin",
+#				"surname":"fowler",
+#				"authorId":1
+#				}
+#			},
+#	"isbn":"lalala",
+#	"publisher":{
+#				"publisherName":"random-house"
+#			},
+#	"publishedDate":2024-03-17,
+#	"edition":1,
+#	"form":"HARDCOVER"
+#}
+
+
+#curl -iv localhost:8080/rest/library/addBook -H "Content-Type:application/json" -d "{\"book\":{\"title\":\"effective java\",\"author\":{\"name\":\"martin\",\"surname\":\"fowler\",\"authorId\":1}},\"isbn\":\"lalala\",\"publisher\":{\"publisherName\":\"random-house\"},\"publishedDate\":\"2024-03-17\",\"edition\":1,\"form\":\"HARDCOVER\"}"
+
+
+
+curl -iv localhost:8080/rest/library/addBook -H "Content-Type:application/json" -d "{\"book\":{\"title\":\"effective java\",\"author\":{\"name\":\"martin\",\"surname\":\"fowler\",\"authorId\":1}},\"isbn\":\"lalala\",\"publishedDate\":\"2024-03-17\",\"edition\":1,\"form\":\"HARDCOVER\"}"
+
+curl -iv -X GET localhost:8080/rest/library/listBooks -H "Content-Type:application/json" -d "{\"name\":\"martin\",\"surname\":\"fowler\",\"authorId\":1}"
