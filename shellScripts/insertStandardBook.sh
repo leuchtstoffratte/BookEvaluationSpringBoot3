ea1111bd54103fb9972664563f14e85d41339653
leuchtstoffratte <kitersquirel@web.de>
2025-02-12 09:13:49 +0100

    Fixed author name

--- shellScripts/insertStandardBook.sh
+++ shellScripts/insertStandardBook.sh
@@ -22,6 +22,6 @@
 
 
 
-curl -iv localhost:8080/rest/library/addBook -H "Content-Type:application/json" -d "{\"book\":{\"title\":\"effective java\",\"author\":{\"name\":\"martin\",\"surname\":\"fowler\",\"authorId\":1}},\"isbn\":\"lalala\",\"publishedDate\":\"2024-03-17\",\"edition\":1,\"form\":\"HARDCOVER\"}"
+curl -iv localhost:8080/rest/library/addBook -H "Content-Type:application/json" -d "{\"book\":{\"title\":\"effective java\",\"author\":{\"name\":\"Joshua\",\"surname\":\"Bloch\",\"authorId\":1}},\"isbn\":\"lalala\",\"publishedDate\":\"2024-03-17\",\"edition\":1,\"form\":\"HARDCOVER\"}"
 
-curl -iv -X GET localhost:8080/rest/library/listBooks -H "Content-Type:application/json" -d "{\"name\":\"martin\",\"surname\":\"fowler\",\"authorId\":1}"
+curl -iv -X GET localhost:8080/rest/library/listBooks -H "Content-Type:application/json" -d "{\"name\":\"Joshua\",\"surname\":\"Bloch\",\"authorId\":1}"
