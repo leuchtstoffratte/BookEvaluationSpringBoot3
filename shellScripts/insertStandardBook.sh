41c93a33379726a3f6ca3f56b960fd6e294e62b7
leuchtstoffratte <leuchtstoffratte@10.160.148.195>
2025-02-18 13:09:29 +0100

    Removed books-Section form the authors jsons in curl-scripts

--- shellScripts/insertStandardBook.sh
+++ shellScripts/insertStandardBook.sh
@@ -2,26 +2,25 @@
 #{
 #	"book":{
 #			"title":"effective java",
-#			"author":{
-#				"name":"martin",
-#				"surname":"fowler",
-#				"authorId":1
-#				}
+#			"authors":[
+#                                   {
+#                                       "name":"Joshua",
+#                                       "surname":"Bloch",
+#                                       "authorId":1
+#                                   }
+#                               ]
 #			},
 #	"isbn":"lalala",
-#	"publisher":{
-#				"publisherName":"random-house"
-#			},
 #	"publishedDate":2024-03-17,
 #	"edition":1,
 #	"form":"HARDCOVER"
 #}
 
 
-#curl -iv localhost:8080/rest/library/addBook -H "Content-Type:application/json" -d "{\"book\":{\"title\":\"effective java\",\"author\":{\"name\":\"martin\",\"surname\":\"fowler\",\"authorId\":1}},\"isbn\":\"lalala\",\"publisher\":{\"publisherName\":\"random-house\"},\"publishedDate\":\"2024-03-17\",\"edition\":1,\"form\":\"HARDCOVER\"}"
+#curl -iv localhost:8080/rest/library/addBook -H "Content-Type:application/json" -d "{\"book\":{\"title\":\"effective java\",\"author\":{\"name\":\"Joshua\",\"surname\":\"Bloch\",\"authorId\":1}},\"isbn\":\"lalala\",\"publisher\":{\"publisherName\":\"random-house\"},\"publishedDate\":\"2024-03-17\",\"edition\":1,\"form\":\"HARDCOVER\"}"
 
 
 
-curl -iv localhost:8081/rest/library/addBook -H "Content-Type:application/json" -d "{\"book\":{\"title\":\"effective java\",\"author\":{\"name\":\"Joshua\",\"surname\":\"Bloch\",\"authorId\":1}},\"isbn\":\"lalala\",\"publishedDate\":\"2024-03-17\",\"edition\":1,\"form\":\"HARDCOVER\"}"
+curl -iv localhost:8081/rest/library/addBook -H "Content-Type:application/json" -d "{\"book\":{\"title\":\"effective java\",\"authors\":[{\"name\":\"Joshua\",\"surname\":\"Bloch\",\"authorId\":1}]},\"isbn\":\"lalala\",\"publishedDate\":\"2024-03-17\",\"edition\":1,\"form\":\"HARDCOVER\"}"
 
 curl -iv -X GET localhost:8081/rest/library/listBooks -H "Content-Type:application/json" -d "{\"name\":\"Joshua\",\"surname\":\"Bloch\",\"authorId\":1}"
